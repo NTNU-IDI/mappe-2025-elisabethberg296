5cf61b78ca817ceb6b40a58d628b84fe61e38eb9
Elisabeth Berg <eli.berg296@gmail.com>
2025-12-09 11:46:45 +0100

    Removed a line

--- src/test/java/com/diary/DiaryClientTest.java
+++ src/test/java/com/diary/DiaryClientTest.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
